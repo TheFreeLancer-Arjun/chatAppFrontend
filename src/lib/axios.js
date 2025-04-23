0f664f1e4038e06e154f43f048ef67a2c7c1a4c2
TheFreeLancer-Arjun <ajinkyashinde7756@gmail.com>
2025-04-23 07:34:38 +0100

    allgood

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const axiosInstance = axios.create({
-    baseURL:"http://localhost:5000/api",
+    baseURL:"http://ec2-13-48-25-139.eu-north-1.compute.amazonaws.com/api",
     withCredentials:true,
 
 })
