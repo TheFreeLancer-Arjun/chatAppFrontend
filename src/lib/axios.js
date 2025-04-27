d0226a53e68a7fcf4a899a2754f9cbb97c8e23cf
TheFreeLancer-Arjun <ajinkyashinde7756@gmail.com>
2025-04-27 11:59:39 +0100

    okok

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const axiosInstance = axios.create({
-    baseURL:"https://chatappbackend-81ht.onrender.com",
+    baseURL:"https://chatappbackend-81ht.onrender.com/api",
     withCredentials:true,
 
 })
